88baea2ae001709db74447907088b471f52613f1
Daniel Stefanik <danielstefanik36@gmail.com>
2017-10-21 16:12:53 +0200

    init db created

--- src/main/java/com/designPatterns/ACLDatabaseSecurity/model/entity/Salary.java
+++ src/main/java/com/designPatterns/ACLDatabaseSecurity/model/entity/Salary.java
@@ -1,18 +1,29 @@
 package com.designPatterns.ACLDatabaseSecurity.model.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 
 @Entity
 public class Salary {
 
 	private long salaryId;
 	private double salaryValue;
-	
+
+	// @Id
+	// @GeneratedValue(strategy = GenerationType.AUTO)@Id
+	// @GeneratedValue(strategy = GenerationType.SEQUENCE,
+	// generator="salaries_id_seq")
+	// @SequenceGenerator(name="salaries_id_seq", sequenceName="salaries_id_seq",
+	// allocationSize=1)
+	// @Column(name = "SALARY_ID")
+	// @Id
+	// @GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue
 	public long getSalaryId() {
 		return salaryId;
 	}
