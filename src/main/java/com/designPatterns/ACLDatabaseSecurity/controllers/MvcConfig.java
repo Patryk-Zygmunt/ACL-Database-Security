3fe76193415ee567450339759afee93451476f10
patryk-zygmunt <patrykzygmuntzx@gmail.com.com>
2018-01-07 14:40:30 +0100

    added failure login page, logout and fixes few other things

--- src/main/java/com/designPatterns/ACLDatabaseSecurity/controllers/MvcConfig.java
+++ src/main/java/com/designPatterns/ACLDatabaseSecurity/controllers/MvcConfig.java
@@ -14,6 +14,7 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/failure").setViewName("failure");
     }
 
 }
